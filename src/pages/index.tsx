d0008a076ed8e47e6af4f2394724ae14bc9ab6c8
Henrique <henrique.garcia@leanonsystems.com>
2021-07-27 12:59:19 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>Teste </h1>
+    </div>
+  );
+}
